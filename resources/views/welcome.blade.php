43370206badba557d09e6dddeb4be448b5be623e
asier <asierapraiz@gmail.com>
2018-01-26 13:07:48 +0100

    Hola amigo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Hola
+                    Hola amigo
                 </div>
 
                 <div class="links">
