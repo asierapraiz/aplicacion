d15ddd040a5a0e2ceb0aff8fef5361b8b244e413
asier <asierapraiz@gmail.com>
2018-01-26 15:29:31 +0100

    rama1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Hola desde prueba
+                    Rama1
                 </div>
 
                 <div class="links">
